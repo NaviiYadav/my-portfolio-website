23a33a27457ab532035b9d69c792b5b16089dabf
Naveen Yadav <114905989+NaviiYadav@users.noreply.github.com>
2025-06-20 17:02:32 +0530

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// **
-//    * Toggle mobile nav dropdowns
-//    */
-  document.querySelectorAll('.navbar .navlink ').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      
-      e.stopImmediatePropagation();
+
+   /**
+   * Init typed.js
+   */
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
     });
-  });
+  }
+
